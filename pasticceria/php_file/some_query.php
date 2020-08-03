e5765e21bdedc4aa383c53961c273cc73e24126b
cresponzio <pampero.especial@hotmail.it>
2020-08-03 18:56:58 +0200

    Update some_query.php

--- pasticceria/php_file/some_query.php
+++ pasticceria/php_file/some_query.php
@@ -6,7 +6,7 @@
 			$host = '127.0.0.1';
 			$db_name = 'pasticceria';
 			$db_user = 'phpmyadmin';  
-			$db_password = 'labbradoR2!';  
+			$db_password = '';  
 		    return new PDO('mysql:host='.$host.';dbname='.$db_name, $db_user, $db_password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		}
 
